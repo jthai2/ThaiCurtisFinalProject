7b840438302708e2d5ade354ad0789d60a09dee2
jthai2 <jimmythai35@gmail.com>
2017-10-11 09:17:49 -0500

    update

--- Final Project/src/allGames/ObstacleCourse.java
+++ Final Project/src/allGames/ObstacleCourse.java
@@ -3,14 +3,12 @@ package allGames;
 import java.util.Scanner;
 
 public class ObstacleCourse {
-	
-	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
 		String choiceString; 
-//		int questionNum;
-//		int [] questionNumArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
 		System.out.println("Welcome to this game, you will need to complete 10 questions to pass this course. "
 				+ "\nPay attention to every detail because you might get answers from it. \nIf you don't get the answer right in 3 attempts,"
@@ -29,6 +27,8 @@ public class ObstacleCourse {
 	}
 		public static void questions1()
 {
+		Scanner input = new Scanner(System.in);
+		
 		String choiceString; 
 		int question1 = 6;
 		
@@ -47,14 +47,14 @@ public class ObstacleCourse {
 		
 		public static void question2()
 {
+			Scanner input = new Scanner(System.in);
+			
 			String choiceString;
 			
-			System.out.println("There was an airplane crash, every single person on board died, but yet two people survived. \nHow is this possible?>> ");
+			System.out.println("Do bald people shampoo their head?>> ");
 			choiceString = input.next();
 			
-			if("The two were married".equalsIgnoreCase(choiceString) || "The 2 were married".equalsIgnoreCase(choiceString) 
-					|| "2 were married".equalsIgnoreCase(choiceString) || "Two were married".equalsIgnoreCase(choiceString)
-					|| "They were married".equalsIgnoreCase(choiceString))
+			if("Yes".equalsIgnoreCase(choiceString))
 			{
 				question3();
 			}
@@ -62,13 +62,33 @@ public class ObstacleCourse {
 			{
 				System.out.println("Wrong, start over...");
 			}
+
 }
 		public static void question3()
 		{
+			Scanner input = new Scanner(System.in);
 			String choiceString;
 			
-			System.out.println("Test question>>");
+			System.out.println("Is sand called sand because it's between sea and land? 'YES' or 'NO'>> ");
 			choiceString = input.next();
-		
+			
+			if("No".equalsIgnoreCase(choiceString))
+			{
+				System.out.println("Right \nThe word sand is thought to have originated from an Old English word, which itself "
+						+ "originated from the old Dutch word sant, which became zand"); 
+			question4();	
+			}
+			else
+			{
+				System.out.println("Wrong, start over...");
+			}
+		}
+		public static void question4()
+		{
+			Scanner input = new Scanner(System.in);
+			String choiceString;
+			
+			System.out.println("Test question");
+			choiceString = input.next();
 		}
 }
