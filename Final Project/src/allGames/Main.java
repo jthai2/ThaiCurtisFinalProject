db3beb56741a9e667d6de780b7f2aff38caa9ba5
jthai2 <jimmythai35@gmail.com>
2017-10-18 08:13:31 -0500

    update

--- Final Project/src/allGames/Main.java
+++ Final Project/src/allGames/Main.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
-		System.out.println("1. ObsatcleCourse \n2. TicTacToe \n3. War \nChoose your game>>");
-		int game = input.nextInt();
 		
-		if(game == 1)
+		System.out.println("1. ObsatcleCourse \n2. TicTacToe \n3. War \nChoose your game>>");
+		int choice = input.nextInt();
+		
+		if(choice == 1)
 		{
 			ObstacleCourse.main(args);
 		}
-		if(game == 2)
+		if(choice == 2)
 		{
 			TicTacToe.main(args);
 		}
-		if(game == 3) {
+		if(choice == 3) {
 			War.main(args);
 		}
 	}
